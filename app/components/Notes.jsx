9c5ed6e653d2e227f298537f6a7c78b970ac6e87
Jose Reyes <jore5523@gmail.com>
2017-09-19 16:51:41 -0400

    change the Notes model

---
+++ app/components/Notes.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Note from './Note';
+
+
+
+export default ({notes}) => (
+  <ul>{notes.map(note =>
+    <li key={note.id}><Note task={note.task} /></li>
+  )}</ul>
+)
--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import App from './components/App';
+//import Notes from './components/Notes';
+
 if(process.env.NODE_ENV !== 'production') {
   React.Perf = require('react-addons-perf');
 }
 
 ReactDOM.render(
-  <div>Hello world</div>,
+  //<div>Hello world</div>,
+  //<Notes />,
+  <App />,
   document.getElementById('app')
 );
